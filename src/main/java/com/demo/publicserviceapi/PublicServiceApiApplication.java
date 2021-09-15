491eb5196fb82054c0f24faf186f291e0bbb9829
Oitejjho Dutta <oitejjhodutta@AGC-FVFD703AP3YV.local>
2021-09-15 23:08:27 +0700

    gitingore added.

---
+++ src/main/java/com/demo/publicserviceapi/PublicServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.publicserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/publicserviceapi/PublicServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.publicserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
